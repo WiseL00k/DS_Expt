f5f4648c8a8ff98df08862ca080076fc1c33ddbc
WiseLook <1656438881@qq.com>
2024-11-25 16:55:42 +0800

    Defined MFSet API in DSU.h.

--- inc/DSU.h
+++ inc/DSU.h
@@ -1,6 +1,21 @@
 #ifndef __DSU_H__
 #define __DSU_H__
 
+typedef enum {
+    FALSE,
+    TRUE,
+    OVERFLOW,
+} Status;
 
+typedef struct {
+    int* parent;
+    int n;
+}PForest, MFSet;
 
-#endif
+Status InitMFSet(MFSet *S, int n);
+Status DestroyMFSet(MFSet *S);
+int FindMFset(MFSet *S, int i);
+Status DiffMFSet(MFSet *S, int i, int j);
+Status UnionMFSet(MFSet *S, int i, int j);
+
+#endif /* __DSU_H__ */
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "DSU.h"
 
 int main(void)
 {
