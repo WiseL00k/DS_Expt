146a7f9265e0cfdc0ae14eb2f7b3fd983c2ba7e3
WiseLook <1656438881@qq.com>
2024-12-01 14:18:34 +0800

    Define API of UDGraph.

--- inc/UDGraph.h
+++ inc/UDGraph.h
@@ -12,7 +12,11 @@
 #define MAXINT 0xFFFFFFFF
 
 typedef char VexType;
-
+typedef struct
+{
+    VexType v, w;
+    int info;
+} ArcInfo;
 typedef enum
 {
     ERROR = -1,
@@ -21,7 +25,6 @@ typedef enum
     OK,
     OVERFLOW,
 } Status; // 状态码
-
 typedef enum
 {
     DG,
@@ -35,11 +38,23 @@ typedef struct
 {
     VexType *vexs;
     int **arcs;
-
     int n, e;
     GraphKind kind;
     int *tags;
 } MGraph; // 邻接数组类型
+
+Status InitGraph_M(MGraph *G, GraphKind kind, VexType *vexs, int n);
+Status CreateGraph_M(MGraph *G, GraphKind kind, VexType *vexs, int n, ArcInfo *arcs, int e);
+Status DestroyGraph_M(MGraph *G);
+int LocateVex_M(MGraph G, VexType v);
+Status GetVex_M(MGraph G, int k, VexType *w);
+Status PutVex_M(MGraph G, int k, VexType w);
+int FirstAdjVex_M(MGraph G, int k);
+int NextAdjVex_M(MGraph G, int k, int m);
+Status AddArc_M(MGraph *G, int k, int m, int info);
+Status RemoveArc_M(MGraph *G, int k, int m);
+Status DFSTraverse_M(MGraph G, Status (*visit)(int)); // 深度优先遍历图G
+Status BFSTraverse_M(MGraph G, Status (*visit)(int)); // 广度优先遍历图G
 
 #endif
 
@@ -56,7 +71,6 @@ typedef struct VexNode
     VexType data;
     struct AdjVexNode *firstArc;
 } VexNode;
-
 typedef struct
 {
     VexNode *vexs;
@@ -64,6 +78,18 @@ typedef struct
     GraphKind kind;
     int *tags;
 } ALGraph; // 邻接表类型
+
+Status CreateGraph_AL(ALGraph *G, GraphKind kind, VexType *vexs, int n, ArcInfo *arcs, int e);
+Status DestroyGraph_AL(ALGraph *G); // 销毁图G
+Status LocateVex_AL(ALGraph G, VexType v);
+Status GetVex_AL(ALGraph G, int k, VexType *w);
+Status PutVex_AL(ALGraph G, int k, VexType w);
+int FirstAdjVex_AL(ALGraph G, int k, AdjVexNodeP *p);
+int NextAdjVex_AL(ALGraph G, int k, AdjVexNodeP *p);
+Status AddArc_AL(ALGraph *G, int k, int m, int info);
+Status RemoveArc_AL(ALGraph *G, int k, int m);
+Status DFSTraverse_AL(ALGraph G, Status (*visit)(int)); // 深度优先遍历图G
+Status BFSTraverse_AL(ALGraph G, Status (*visit)(int)); // 广度优先遍历图G
 
 #endif
 
